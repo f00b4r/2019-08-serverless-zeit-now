9db7adbd068a7b895a669be102f1d0ef027a6775
Milan Felix Šulc <rkfelix@gmail.com>
2019-08-23 22:32:48 +0200

    Init

---
+++ 4-web/web/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Hello Nette Camp 2019!</h1>
+
+    <button @click.prevent="onLoad">Load users</button>
+
+    <ul>
+      <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    users: []
+  }),
+  methods: {
+    async onLoad() {
+      const res = await fetch("/api/users/");
+      const data = await res.json();
+      this.users = data;
+    }
+  }
+};
+</script>
---
+++ README.md
@@ -0,0 +1,12 @@
+# ZEIT Now + now-php
+
+- Author: Felix (https://f3l1x.io)
+- ZEIT Now (https://zeit.co)
+- PHP builder - `now-php` - (https://github.com/juicyfx/now-php)
+
+## Examples
+
+1. Static deployment
+2. Node deployment
+3. PHP deployment
+4. PHP + composer deployment
